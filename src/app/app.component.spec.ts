7fb50f963d493ec3d5e77ec485078619009635d1
Starlynne <lewis.start0905@gmail.com>
2018-11-01 12:17:27 -0700

    Basic scaffold, add game and button components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'simon'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('simon');
+    expect(app.title).toEqual('Simon');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to simon!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Simon!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { GameButtonComponent } from './components/game/game-button/game-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    GameButtonComponent
   ],
   imports: [
     BrowserModule
